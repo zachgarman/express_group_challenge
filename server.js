ea5900187b1e7142b74a4245cf8811a5999fd268
Zach Garman <zachgarman@gmail.com>
2016-10-07 10:27:31 -0500

    all routers working

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var bios = require('./public/assets/bios/bios');  // this file must be accessed by biosRouter
+var bios = require('./public/assets/bios/bios');  
 var likesRouter = require('./routes/likes');
-//var biosRouter = require('./routes/bios');  //router won't work while needing bios.json fil
+var biosRouter = require('./routes/bios');
 
 var app = express();
 
-//middleware running
+// middleware running
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// routers running
 app.use('/likes', likesRouter);
-//app.use('/bios', biosRouter);  // can't use biosRouter without access to bios.json
+app.use('/bios', biosRouter);
 
 app.get('/', function (req, res) {
   var fileName = path.join(__dirname, 'public/views/index.html');
   res.sendFile(fileName);
 });
 
-app.get('/bios', function (req, res) {  // can't move this to biosRouter, how can we?
-  res.send(bios);
-});
 
 app.listen(3000);
