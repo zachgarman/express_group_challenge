20f7b93d2c131e9746aa6f5920c240c904583e95
Zach Garman <zachgarman@gmail.com>
2016-10-06 21:30:31 -0500

    Router for bios commented out

---
+++ routes/bios.js
@@ -0,0 +1,11 @@
+// var express = require('express');
+// // need access to the bios.json file in this router
+// var bios = require('./public/assets/bios/bios');
+//
+// var router = express.Router();
+// // send back bios.json file when request to bios comes here
+// router.get('/', function (req, res) {
+//   res.send(bios);
+// });
+//
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var bios = require('./public/assets/bios/bios');
+var bios = require('./public/assets/bios/bios');  // this file must be accessed by biosRouter
 var likesRouter = require('./routes/likes');
+//var biosRouter = require('./routes/bios');  //router won't work while needing bios.json fil
 
 var app = express();
 
@@ -11,13 +12,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/likes', likesRouter);
+//app.use('/bios', biosRouter);  // can't use biosRouter without access to bios.json
 
 app.get('/', function (req, res) {
   var fileName = path.join(__dirname, 'public/views/index.html');
   res.sendFile(fileName);
 });
 
-app.get('/bios', function (req, res) {
+app.get('/bios', function (req, res) {  // can't move this to biosRouter, how can we?
   res.send(bios);
 });
 
